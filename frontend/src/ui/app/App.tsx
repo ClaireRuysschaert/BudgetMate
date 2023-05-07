3102caf1b8a696db8e664cacc6b6191b472a7131
Shriukan33 <shriukan@gmail.com>
2023-05-07 17:07:05 +0200

    Add basic form to make sure API is working

--- frontend/src/ui/app/App.tsx
+++ frontend/src/ui/app/App.tsx
@@ -1,6 +1,28 @@
+import { useState } from 'react';
+import { api } from 'ui/services/ApiService';
+
 const App = () => {
+
+  const [message, setMessage] = useState<string>('');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    api.get('/dummy/').then((res) => {
+      console.log(res)
+      setMessage(res.message)
+    })
+  }
+
   return (
-    <div>Hello, World!</div>
+    <main>
+    <h1>BudgetMate</h1>
+    <div>
+        <form method="GET" onSubmit={handleSubmit}>
+          <input type="submit" value="Send GET request to /api/dummy/" />
+        </form>
+        <p>{message}</p>
+      </div>
+    </main>
   )
 }
 
